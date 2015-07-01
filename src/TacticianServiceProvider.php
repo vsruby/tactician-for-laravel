f1763c742d53d419290779b6af2323ba62c19183
Vince Ruby <vsruby91@gmail.com>
2015-07-01 12:51:39 -0400

    Bind dispatcher to interface

--- src/TacticianServiceProvider.php
+++ src/TacticianServiceProvider.php
@@ -26,5 +26,12 @@ class TacticianServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'tactician');
+
+        $this->registerDispatcher();
+    }
+
+    protected function registerDispatcher()
+    {
+    	$this->app->bind('VinceRuby/Tactician/Contracts/Bus/Dispatcher', 'VinceRuby/Tactician/Dispatcher');
     }
 }
